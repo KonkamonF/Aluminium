7e36c739cde4db0bc36512b756e16470fa9e90f2
KonkamonF <konkamonfungsuk@gmail.com>
2025-02-23 13:31:14 +0700

    Start Header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div className='bg-lime-700'>Header</div>
+    <div className='bg-lime-700 m-5 rounded-t-2xl text-center p-2'>Header</div>
   )
 }
