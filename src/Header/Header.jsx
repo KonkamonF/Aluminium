4d72ab2bdb670dcf09dc7668ef6477322b0e14e5
KonkamonF <konkamonfungsuk@gmail.com>
2025-02-24 10:11:24 +0700

    logo

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import Logo from "../assets/PG.png";
 
 export default function Header() {
   return (
-    <div className="bg-lime-700 m-5 rounded-t-2xl text-center p-2 text-white">
+    <div className="bg-[#004d21] m-5 rounded-t-2xl text-center p-2 text-white">
+      <img src={Logo} alt="" className="w-40" />
       <p className="text-4xl">ร้านพงษ์เจริญ กระจกอลูมิเนียม สากเหล็ก พิจิตร</p>
-      <p className="text-3xl pt-4">รับติดตั้ง กั้นห้อง ซ่อมแซ่ม กระจกอลูมิเนียม</p>
+      <p className="text-3xl pt-4">
+        รับติดตั้ง กั้นห้อง ซ่อมแซ่ม กระจกอลูมิเนียม
+      </p>
+      <p className="text-4xl pt-4">โทร 081-973-3078</p>
     </div>
   );
 }
