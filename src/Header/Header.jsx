c96a229cdd3e18c0cb9cb0af4ad2f79024513223
KonkamonF <konkamonfungsuk@gmail.com>
2025-02-24 10:03:31 +0700

    continue Header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
 
 export default function Header() {
   return (
-    <div className='bg-lime-700 m-5 rounded-t-2xl text-center p-2'>Header</div>
-  )
+    <div className="bg-lime-700 m-5 rounded-t-2xl text-center p-2 text-white">
+      <p className="text-4xl">ร้านพงษ์เจริญ กระจกอลูมิเนียม สากเหล็ก พิจิตร</p>
+      <p className="text-3xl pt-4">รับติดตั้ง กั้นห้อง ซ่อมแซ่ม กระจกอลูมิเนียม</p>
+    </div>
+  );
 }
