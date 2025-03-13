9716d3a0da6cbf9977a214d17ca493f7b094eb02
KonkamonF <konkamonfungsuk@gmail.com>
2025-03-13 14:11:25 +0700

    edit header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -4,13 +4,13 @@ import Banner from "../assets/Banner.png";
 
 export default function Header() {
   return (
-    <div className="m-6 mx-auto rounded-t-2xl text-center p-2 text-white bg-[#004d21]">
+    <div className="font-bold m-6 mx-auto rounded-t-2xl text-center p-2 text-white bg-[#004d21]">
       <img src={Banner} alt="" className="rounded-t-2xl w-[100%]" />
       <p className="text-4xl pt-4">ร้านพงษ์เจริญ กระจกอลูมิเนียม สากเหล็ก พิจิตร</p>
       <p className="text-3xl pt-4">
         รับติดตั้ง กั้นห้อง ซ่อมแซ่ม กระจกอลูมิเนียม
       </p>
-      <p className="text-4xl pt-2">โทร 081-973-3078</p>
+      <p className="text-4xl pt-3">โทร 081-973-3078</p>
     </div>
   );
 }
