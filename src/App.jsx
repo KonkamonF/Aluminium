3cc21ecbff1169bdab285e36331165eb48e77bfe
KonkamonF <konkamonfungsuk@gmail.com>
2025-03-13 14:08:17 +0700

    start body

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "./Header/Header";
+import Body from "./Body/Body";
 
 export default function App() {
   return (
     <>
-      <Header/>
-      <div>App</div>
+      <Header />
+      <Body />
     </>
   );
 }
