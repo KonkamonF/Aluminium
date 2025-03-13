7713cc031e5fac90a69a4d99e4106038adb35a35
KonkamonF <konkamonfungsuk@gmail.com>
2025-03-13 20:35:49 +0700

    body ja

--- src/Body/Body.jsx
+++ src/Body/Body.jsx
@@ -4,48 +4,50 @@ import side from "../assets/Sidebody.png";
 export default function Body() {
   return (
     <>
-      <div className="text-center text-[#004d21] flex items-center mx-60 pb-4">
-        <img src={side} alt="" className="w-[500px]" />
-        <div className="pl-10">
-          <p className="text-2xl font-bold">
-            บริการติดตั้งกระจกอลูมิเนียม
-            เพิ่มความหรูหราและทันสมัยให้กับบ้านและอาคารของคุณ
-          </p>
-          <p className="text-lg text-start leading-relaxed pt-4">
-            กระจกอลูมิเนียมเป็นวัสดุที่ได้รับความนิยมอย่างมากในงานก่อสร้างและตกแต่ง
-            เนื่องจากมีคุณสมบัติที่โดดเด่น ทั้งในด้านความแข็งแรง ความทนทาน
-            และการออกแบบที่ทันสมัย ซึ่งช่วยเสริมภาพลักษณ์ของบ้าน อาคารสำนักงาน
-            หรือร้านค้าให้ดูโปร่งโล่ง หรูหรา และทันสมัยขึ้น
-          </p>
+      <div className="py-18">
+        <div className="text-center text-[#004d21] flex items-center mx-60 pb-4">
+          <img src={side} alt="" className="w-[50%] rounded-4xl" />
+          <div className="pl-10">
+            <p className="text-2xl font-bold">
+              บริการติดตั้งกระจกอลูมิเนียม
+              เพิ่มความหรูหราและทันสมัยให้กับบ้านและอาคารของคุณ
+            </p>
+            <p className="text-lg text-start leading-relaxed pt-4">
+              กระจกอลูมิเนียมเป็นวัสดุที่ได้รับความนิยมอย่างมากในงานก่อสร้างและตกแต่ง
+              เนื่องจากมีคุณสมบัติที่โดดเด่น ทั้งในด้านความแข็งแรง ความทนทาน
+              และการออกแบบที่ทันสมัย ซึ่งช่วยเสริมภาพลักษณ์ของบ้าน อาคารสำนักงาน
+              หรือร้านค้าให้ดูโปร่งโล่ง หรูหรา และทันสมัยขึ้น
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="text-center text-[#004d21] text-lg leading-relaxed flex items-center mx-60">
-        <div className="pr-10">
-          <p className="text-2xl font-bold pb-4">
-            ทำไมต้องเลือกกระจกอลูมิเนียม?
-          </p>
-          <p className="text-start">
-            <span className="font-bold">แข็งแรงและทนทาน - </span>
-            อลูมิเนียมเป็นวัสดุที่ไม่เป็นสนิมและสามารถทนต่อสภาพอากาศได้ดี
-            เหมาะสำหรับการใช้งานทั้งภายในและภายนอก
-          </p>
-          <p className="text-start">
-            <span className="font-bold">ดีไซน์ทันสมัย - </span>
-            กระจกอลูมิเนียมช่วยให้บ้านหรืออาคารของคุณดูโปร่งแสงและโมเดิร์น
-            เพิ่มความสวยงามและสไตล์ที่เป็นเอกลักษณ์
-          </p>
-          <p className="text-start">
-            <span className="font-bold">ลดเสียงรบกวน - </span>
-            กระจกอลูมิเนียมคุณภาพสูงสามารถช่วยลดเสียงรบกวนจากภายนอก
-            ทำให้บรรยากาศภายในเงียบสงบมากขึ้น
-          </p>
-          <p className="text-start">
-            <span className="font-bold">ดูแลรักษาง่าย - </span>
-            วัสดุอลูมิเนียมและกระจกสามารถทำความสะอาดได้ง่าย
-            ไม่ต้องกังวลเรื่องเชื้อรา หรือการผุพังเหมือนไม้
-          </p>
+        <div className="text-center text-[#004d21] text-lg leading-relaxed flex items-center mx-60">
+          <div className="pr-10">
+            <p className="text-2xl font-bold pb-4">
+              ทำไมต้องเลือกกระจกอลูมิเนียม?
+            </p>
+            <p className="text-start">
+              <span className="font-bold">แข็งแรงและทนทาน - </span>
+              อลูมิเนียมเป็นวัสดุที่ไม่เป็นสนิมและสามารถทนต่อสภาพอากาศได้ดี
+              เหมาะสำหรับการใช้งานทั้งภายในและภายนอก
+            </p>
+            <p className="text-start">
+              <span className="font-bold">ดีไซน์ทันสมัย - </span>
+              กระจกอลูมิเนียมช่วยให้บ้านหรืออาคารของคุณดูโปร่งแสงและโมเดิร์น
+              เพิ่มความสวยงามและสไตล์ที่เป็นเอกลักษณ์
+            </p>
+            <p className="text-start">
+              <span className="font-bold">ลดเสียงรบกวน - </span>
+              กระจกอลูมิเนียมคุณภาพสูงสามารถช่วยลดเสียงรบกวนจากภายนอก
+              ทำให้บรรยากาศภายในเงียบสงบมากขึ้น
+            </p>
+            <p className="text-start">
+              <span className="font-bold">ดูแลรักษาง่าย - </span>
+              วัสดุอลูมิเนียมและกระจกสามารถทำความสะอาดได้ง่าย
+              ไม่ต้องกังวลเรื่องเชื้อรา หรือการผุพังเหมือนไม้
+            </p>
+          </div>
+          <img src={side} alt="" className="w-[50%] rounded-4xl" />
         </div>
-        <img src={side} alt="" className="w-[500px]" />
       </div>
     </>
   );
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -4,15 +4,19 @@ import Banner from "../assets/Banner.png";
 
 export default function Header() {
   return (
-    <div className="font-bold text-center p-4 pb-[8%] text-white bg-[#004d21] flex justify-center items-center flex-col">
-      <img src={Banner} alt="" className="rounded-2xl w-[50%]" />
-      <p className="text-4xl pt-4">
-        ร้านพงษ์เจริญ กระจกอลูมิเนียม สากเหล็ก พิจิตร
-      </p>
-      <p className="text-3xl pt-4">
-        รับติดตั้ง กั้นห้อง ซ่อมแซ่ม กระจกอลูมิเนียม
-      </p>
-      <p className="text-4xl pt-3">โทร 081-973-3078</p>
-    </div>
+    <>
+      <div className="flex items-center flex-col font-bold text-center p-4 text-white bg-[#004d21] h-screen">
+          <img src={Banner} alt="" className="rounded-3xl w-[50%]" />
+        <div className="flex flex-col items-center pt-14">
+          <p className="text-5xl pt-4">
+            ร้านพงษ์เจริญ กระจกอลูมิเนียม สากเหล็ก พิจิตร
+          </p>
+          <p className="text-5xl font-thin pt-4">
+            รับติดตั้ง กั้นห้อง ซ่อมแซ่ม กระจกอลูมิเนียม
+          </p>
+          <p className="text-5xl pt-3">โทร 081-973-3078</p>
+        </div>
+      </div>
+    </>
   );
 }
