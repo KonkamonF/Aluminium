3031c2bdc30ba76a64846d0be0a6ce6b2bc04374
Konkamon Fungsuk <konkamon@sbo-brand.com>
2025-07-30 16:46:21 +0700

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header/Header";
 import Body from "./Body/Body";
+import Footer from "./Footer/Footer";
 
 export default function App() {
   return (
     <>
       <Header />
       <Body />
+      <Footer/>
     </>
   );
 }
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -5,8 +5,8 @@ import Banner from "../assets/Banner.png";
 export default function Header() {
   return (
     <>
-      <div className="flex items-center flex-col font-bold text-center p-4 text-white bg-[#004d21] h-screen">
-          <img src={Banner} alt="" className="rounded-3xl w-[50%]" />
+      <div className="flex items-center flex-col font-bold text-center p-4 text-white bg-[#004d21] ">
+          <img src={Banner} alt="" className="rounded-3xl w-[60%]" />
         <div className="flex flex-col items-center pt-14">
           <p className="text-5xl pt-4">
             ร้านพงษ์เจริญ กระจกอลูมิเนียม สากเหล็ก พิจิตร
@@ -15,7 +15,7 @@ export default function Header() {
             รับติดตั้ง กั้นห้อง ซ่อมแซ่ม กระจกอลูมิเนียม
           </p>
           <p className="text-5xl pt-3">โทร 081-973-3078</p>
-          <p className="text-5xl pt-3">โทร 081-973-3078</p>
+          {/* <p className="text-5xl pt-3">โทร 081-973-3078</p> */}
         </div>
       </div>
     </>
