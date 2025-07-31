6de45acca59ac623b654bbad7821e0b157695c69
Konkamon Fungsuk <konkamon@sbo-brand.com>
2025-07-31 11:28:36 +0700

    footer name

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from "react";
+import { FaLine, FaSquareFacebook, FaMapPin, FaTiktok } from "react-icons/fa6";
 
 export default function Footer() {
   return (
     <>
-    <div className='bg-white'>footer
-      <p className='font-extrabold text-2xl'>PONGCHAROEN</p>
-      <p></p>
-    </div>
+      <div className="bg-white">
+        footer
+        <div className="flex justify-around">
+          <p className="font-extrabold text-4xl">PONGCHAROEN</p>
+          <div className="flex gap-4 text-2xl hover:cursor-pointer">
+            <FaLine color="green" />
+            <FaSquareFacebook color="blue" />
+            <FaMapPin color="purple" />
+            <FaTiktok />
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
