b1b698836a911974c813b54aa75e3aa1c99c03d6
Konkamon Fungsuk <konkamon@sbo-brand.com>
2025-07-31 10:28:46 +0700

    npm react icon

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Footer() {
   return (
     <>
-    <div className='bg-white'>footer</div>
+    <div className='bg-white'>footer
+      <p className='font-extrabold text-2xl'>PONGCHAROEN</p>
+      <p></p>
+    </div>
     </>
   )
 }
