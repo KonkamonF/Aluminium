e606840194e0d99acf765cceb23a8f469ff9bf4f
Konkamon Fungsuk <konkamon@sbo-brand.com>
2025-08-22 15:45:50 +0700

    header edited

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -13,13 +13,13 @@ export default function Header() {
 
       {/* Header Text */}
       <div className="flex flex-col items-center pt-6 px-4">
-        <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl pt-2 leading-tight">
+        <p className="text-2xl sm:text-xl md:text-4xl lg:text-5xl pt-2 leading-tight">
           ร้านพงษ์เจริญ กระจกอลูมิเนียม สากเหล็ก พิจิตร
         </p>
-        <p className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-thin pt-4 leading-snug">
+        <p className="text-xl sm:text-xl md:text-3xl lg:text-4xl font-thin pt-4 leading-snug">
           รับติดตั้ง กั้นห้อง ซ่อมแซ่ม กระจกอลูมิเนียม
         </p>
-        <p className="text-xl sm:text-2xl md:text-3xl lg:text-4xl pt-3 leading-snug">
+        <p className="text-xl sm:text-xl md:text-3xl lg:text-4xl pt-3 leading-snug">
           โทร 081-973-3078
         </p>
       </div>
