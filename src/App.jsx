9960f893ddcb622799d62e6a0243c890bc61a2dd
KonkamonF <konkamonfungsuk@gmail.com>
2025-08-23 15:24:26 +0700

    add app

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./Header/Header";
 import Body from "./Body/Body";
 import Footer from "./Footer/Footer";
+import SlideBody from "./Body/SlideBody";
 
 export default function App() {
   return (
     <>
       <div className="font-Athiti">
         <Header />
+        <SlideBody/>
         <Body />
         <Footer />
       </div>
