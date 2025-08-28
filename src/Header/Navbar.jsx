6439f8dd7c8b07cfbfdab6fb103953a98a429439
Konkamon Fungsuk <konkamon@sbo-brand.com>
2025-08-28 14:11:50 +0700

    navbar bold

--- src/Header/Navbar.jsx
+++ src/Header/Navbar.jsx
@@ -3,19 +3,21 @@ import React from "react";
 export default function Navbar() {
   return (
     <>
-      <div className="flex justify-center p-2 font-medium">
-        <div className="border-2 rounded-full px-20 py-2 flex flex-row gap-12">
+      <div className="flex justify-center p-2 font-medium bg-[#004d21] text-amber-600">
+        <div className="border-2 rounded-full flex flex-row border-amber-600">
           <div
             dir="ltr"
-            className="hover:cursor-pointer hover:bg-amber-500 rounded-s-lg"
+            className="hover:cursor-pointer hover:bg-amber-600 rounded-s-full px-20 py-2 hover:text-white hover:font-bold"
           >
             Home
           </div>
-          <div className="hover:cursor-pointer hover:bg-amber-500">Service</div>
+          <div className="hover:cursor-pointer hover:bg-amber-600 px-20 py-2 hover:text-white hover:font-bold">
+            Service
+          </div>
 
           <div
             dir="rtl"
-            className="hover:cursor-pointer hover:bg-amber-500 rounded-s-lg"
+            className="hover:cursor-pointer hover:bg-amber-600 rounded-s-full px-20 py-2 hover:text-white hover:font-bold"
           >
             Contact
           </div>
