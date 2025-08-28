21473098ef4621b6e9f0f26ff9b50305b2366f3b
Konkamon Fungsuk <konkamon@sbo-brand.com>
2025-08-28 13:24:58 +0700

    done

--- src/Header/Navbar.jsx
+++ src/Header/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar() {
   return (
     <>
-      <div className="flex justify-center gap-4">
+      <div className="flex justify-center gap-4 border-2 rounded-full p-4">
         <div>Home</div>
         <div>Service</div>
         <div>Contact</div>
