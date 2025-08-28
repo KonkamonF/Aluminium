32771497804b5d3cf1a713dd30f4147f7b90e8c9
Konkamon Fungsuk <konkamon@sbo-brand.com>
2025-08-28 13:47:54 +0700

    done

--- src/Header/Navbar.jsx
+++ src/Header/Navbar.jsx
@@ -3,10 +3,23 @@ import React from "react";
 export default function Navbar() {
   return (
     <>
-      <div className="flex justify-center gap-4 border-2 rounded-full p-4">
-        <div>Home</div>
-        <div>Service</div>
-        <div>Contact</div>
+      <div className="flex justify-center p-2 font-medium">
+        <div className="border-2 rounded-full px-20 py-2 flex flex-row gap-12">
+          <div
+            dir="ltr"
+            className="hover:cursor-pointer hover:bg-amber-500 rounded-s-lg"
+          >
+            Home
+          </div>
+          <div className="hover:cursor-pointer hover:bg-amber-500">Service</div>
+
+          <div
+            dir="rtl"
+            className="hover:cursor-pointer hover:bg-amber-500 rounded-s-lg"
+          >
+            Contact
+          </div>
+        </div>
       </div>
     </>
   );
